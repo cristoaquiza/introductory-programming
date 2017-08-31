55a9db27fbaf9b02ebba0722394b296ed3ab39dc
Cristian <cristoaquizal@gmail.com>
2017-08-31 11:26:43 -0500

    Draw a right triangle

--- src/com/tw/ctoaqui/TriangleExercises.java
+++ src/com/tw/ctoaqui/TriangleExercises.java
@@ -5,7 +5,10 @@ public class TriangleExercises {
     public static void main(String[] args) {
         int n = 3;
         for (int i = 0; i < n; i++) {
-            System.out.println("*");
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
