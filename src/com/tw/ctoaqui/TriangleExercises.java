ff6a86a6237e6b0f9afef3464c8270bf75304cd4
Cristian <cristoaquizal@gmail.com>
2017-08-31 11:25:06 -0500

    Draw a vertical line

--- src/com/tw/ctoaqui/TriangleExercises.java
+++ src/com/tw/ctoaqui/TriangleExercises.java
@@ -3,9 +3,9 @@ package com.tw.ctoaqui;
 public class TriangleExercises {
 
     public static void main(String[] args) {
-        int n = 8;
+        int n = 3;
         for (int i = 0; i < n; i++) {
-            System.out.print("*");
+            System.out.println("*");
         }
     }
 }
