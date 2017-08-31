eb0c6d845404d8a13cf179d2b21559db22f8ccaf
Cristian <cristoaquizal@gmail.com>
2017-08-31 14:55:07 -0500

    Diamond

--- src/com/tw/ctoaqui/DiamondExercises.java
+++ src/com/tw/ctoaqui/DiamondExercises.java
@@ -4,7 +4,7 @@ public class DiamondExercises {
 
     public static void main (String[] args) {
         int n = 3;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             for (int j = n-i; j > 1; j--) {
                 System.out.print(" ");
             }
@@ -12,6 +12,19 @@ public class DiamondExercises {
                 System.out.print("*");
             }
             for (int l = 0; l < i+1; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int m = 0; m < n; m++) {
+            for (int p = 1; p < m+1; p++) {
+                System.out.print(" ");
+            }
+            for (int q = 0; q < (n-1)-m; q++) {
+                System.out.print("*");
+            }
+            for(int r = n; r > m; r--) {
                 System.out.print("*");
             }
             System.out.println();
